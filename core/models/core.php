1797b8472949a61ecdb5585d154dd223f9dd8e23
diego burlando <diegoeburlando@gmail.com>
2017-08-31 23:44:07 -0430

    Primer commit de proyecto PHP avanzado con Composer

---
+++ core/models/core.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*NUCLEO DE TODA LA APLICACION */
+
+	define('HTML_DIR', 'html/');
+	define('APP_TITLE', 'OcrendBB');
+	define('APP_COPY', 'Copyright &copy; '. date('Y', time()) . ' OcrendSoft')
+	include('vendor/autoload.php');
+ ?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+require('core/models/core.php');
+
+if(isset($_GET['view'])){
+		if(file_exists('core/controllers/' .strtolower($_GET['view']) . 'Controllers.php')){
+			include('core/controllers/' .strtolower($_GET['view']) . 'Controllers.php');
+		}else{
+			include('core/controllers/errorControllers.php');
+	}
+}else{
+		include('core/controllers/indexControllers.php');
+}
+	
+ ?>
